ff0622f970033fa318fc7911519af5f0b0de7325
Angelo Bovino <angelo.bovino@outlook.com>
2016-09-12 19:15:27 -0400

    adding static file server

---
+++ day-1-wk-20/static-file-server.js
@@ -0,0 +1,7 @@
+//Static file server
+var express = require('express');
+var app = express();
+app.use('/', express.static(__dirname + '/public'));
+app.listen(3000, function() {
+  console.log('App running on port 3000!');
+});
